0110e794596432e7b365166b317e87f5a1dcf36f
SiyathandanaNontolwana <1462572@students.wits.ac.za>
2021-03-13 01:30:50 +0200

    Updated movement code
    Made modifications in unity: Parented camera to player to simulate a third person view.
    Script: Updated scripts in order to get player charater movement.

--- 3DPlatformerMovementPrototype/Assets/Scripts/PlayerController.cs
+++ 3DPlatformerMovementPrototype/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    [SerializeField] float moveSpeed = 5.0f;
+    public Rigidbody playerRB;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody>(); //Assign the player  rigidbody in order to influence it's position
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //Fixed update is better for movement
+    private void FixedUpdate()
+    {
+        playerMovement();
+    }
+
+    void playerMovement()
+    {
+        // Add vector coordinates to clean up code later on
+
+
+        playerRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, playerRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
     }
 }
