e0f41fba24ddd3fa932716108d6c4d7fb478cf41
SiyathandanaNontolwana <1462572@students.wits.ac.za>
2021-03-22 16:34:55 +0200

    BattleHandler script added
    Added a battle handler script. Currently only contains player information such as base stats. No actual data manipulation present.

---
+++ Data Design Turn Based Combat/Assets/Scripts/BattleHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState { NEUTRAL, PLAYER, ENEMY }
+
+public class BattleHandler : MonoBehaviour
+{
+    GameState currentTurnState;
+
+
+    //PLAYER STATS
+    public int playerHealth = 0;
+    public int playerDefence = 0;
+    public int playerAccuracy = 0;
+    public int playerMinDamage = 0;
+    public int playerMaxDamage = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTurnState = GameState.PLAYER;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        turnStateTracker();
+    }
+
+    private void turnStateTracker()
+    {
+        if (currentTurnState == GameState.PLAYER)
+        {
+            //IF PLAYER PLAYED CHANGE STATE TO ENEMY
+        }
+
+        else if (currentTurnState == GameState.ENEMY)
+        {
+            //IF ENEMY PLAYED CHANGE STATE TO PLAYER
+
+        }
+    }
+
+    private void playerBattleTracker()
+    {
+        //PLAYER ACTION
+
+    }
+}
