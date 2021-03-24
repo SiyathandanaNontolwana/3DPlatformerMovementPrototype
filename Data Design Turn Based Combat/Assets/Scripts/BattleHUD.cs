280c45b99d7481e1234ebb8287fef8ff8c335b75
SiyathandanaNontolwana <1462572@students.wits.ac.za>
2021-03-24 20:22:45 +0200

    Added three scripts
    Added battle handler, hud and unit scripts.

---
+++ Data Design Turn Based Combat/Assets/Scripts/BattleHUD.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleHUD : MonoBehaviour
+{
+    public Text nameText;
+    public Text minHP;
+    public Text maxHP;
+ 
+
+    public void setHUD(Unit unit)
+    {
+        nameText.text = unit.unitName;
+        maxHP.text = unit.maxHP.ToString();
+        minHP.text = unit.currentHP.ToString();
+    }
+
+    public void SetHP(int hp)
+    {
+   
+    }
+}
---
+++ Data Design Turn Based Combat/Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public enum GameState { START, PLAYER, ENEMY, WIN, LOSS }
+
+public class BattleSystem : MonoBehaviour
+{
+    public GameState currentState;
+
+    Unit playerUnit;
+    Unit enemyUnit;
+
+
+    //INFORMATION FOR INSTANTIATION
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    //HUDS
+
+    public BattleHUD playerHUD;
+    public BattleHUD enemyHUD;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = GameState.START;
+        StartCoroutine(setUp());
+    }
+
+    IEnumerator setUp()
+    {
+        //BATTLE SETUP
+        GameObject playerGameObject = Instantiate(playerPrefab, new Vector3(-6.5F, -2F, 0F), Quaternion.identity);
+        playerUnit = playerGameObject.GetComponent<Unit>();
+
+        GameObject enemyGameObject = Instantiate(enemyPrefab, new Vector3 (6.5F, -2F,0F), Quaternion.identity);
+        enemyUnit = enemyGameObject.GetComponent<Unit>();
+
+        playerHUD.setHUD(playerUnit);
+        enemyHUD.setHUD(enemyUnit);
+
+        yield return new WaitForSeconds(2f);
+
+        currentState = GameState.PLAYER;
+        PlayerTurn();
+    }
+
+    IEnumerator PlayerAttack()
+    {
+        bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+
+        enemyHUD.SetHP(enemyUnit.currentHP);
+
+        yield return new WaitForSeconds(2f);
+
+        if(isDead)
+        {
+            //End Game
+            currentState = GameState.WIN;
+            EndBattle();
+        }
+
+        else
+        {
+            //enemy turn
+            currentState = GameState.ENEMY;
+            StartCoroutine(EnemyTurn());
+        }
+    }
+
+    void EndBattle()
+    {
+        if (currentState == GameState.WIN)
+            Debug.Log("You win");
+        else if (currentState == GameState.LOSS)
+            Debug.Log("You lose");
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        yield return new WaitForSeconds(1f);
+
+        bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+        playerHUD.SetHP(playerUnit.currentHP);
+
+        yield return new WaitForSeconds(1f);
+
+        if(isDead)
+        {
+            currentState = GameState.LOSS;
+        }
+        else
+        {
+            currentState = GameState.PLAYER;
+            PlayerTurn();
+        }
+
+    }
+
+    void PlayerTurn()
+    {
+
+    }
+
+    public void OnAttackButton()
+    {
+        if(currentState != GameState.PLAYER)
+        {
+            return;
+        }
+
+        StartCoroutine(PlayerAttack());
+
+    }
+}
